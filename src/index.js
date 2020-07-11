e8575bf1d9c27c49208780a8d0a7d59dfc4da44f
jiiis <jiiis@Haos-Macbook.hub>
2020-07-11 22:12:04 +1000

    Tested using Graphql.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Subscription from './resolvers/Subscription'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/comment'
+
+import db from './db'
+
+// Scalar types: String, Boolean, Int, Float, ID
+
+const pubSub = new PubSub()
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers: { Query, Mutation, Subscription, User, Post, Comment },
+  context: { db, pubSub }
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
---
+++ src/resolvers/Query.js
@@ -0,0 +1,21 @@
+const Query = {
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users
+    }
+
+    return db.users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
+  },
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts
+    }
+
+    return db.posts.filter(post => post.title.toLowerCase().includes(args.query.toLowerCase()) || post.body.toLowerCase().includes(args.query.toLowerCase()))
+  },
+  comments(parent, args, { db }, info) {
+    return db.comments
+  }
+}
+
+export default Query
